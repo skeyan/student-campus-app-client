f148e74c204de586a69f85dc1d2938a6cfaee08d
Jamswhat2 <hilarionjames25@gmail.com>
2021-12-18 12:14:57 -0500

    implemented edit functionality (containers+view), updated App.js

---
+++ src/components/containers/EditCampusContainer.js
@@ -0,0 +1,192 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+import { EditCampusView } from '../views';
+import { 
+    addCampusThunk,
+    fetchCampusThunk,
+    editCampusThunk 
+} from '../../store/thunks';
+
+class EditCampusContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            imageUrl: "",
+            address: "",
+            description: "",
+            redirect: false,
+            redirectId: null,
+            id: null,
+            errors: {
+                name: null,
+                address: null,
+                validate: null,
+                shouldDisplayError: false
+        }};
+    }
+    /* Handle Focus for UX */
+    handleFocus = () => {
+        if (!this.state.errors.shouldDisplayError) {
+            console.log("Not true");
+            this.setState(prevstate => ({
+                errors: {
+                    ...prevstate.errors,
+                    shouldDisplayError: true
+                }
+            }));
+        }
+    }
+
+    /* Helper Function */
+    isBlank(input) {
+        if (input.trim() === "")
+            return true
+        return false
+    }
+
+    /* Input Validation */
+    /*
+    * The name and address fields are required.
+    */
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value.trim()
+        });
+
+        let nameError = false;
+        let addressError = false;
+
+        switch (event.target.name) {
+            case "name":
+                if (this.isBlank(event.target.value)) {
+                    nameError = true;
+                }
+                if (this.isBlank(this.state.address))
+                    addressError = true;
+                break;
+            case "address":
+                if (this.isBlank(event.target.value))
+                    addressError = true;
+                if (this.isBlank(this.state.name))
+                    nameError = true;
+                break;
+            default:
+                return;
+        }
+
+        this.setState(prevstate => ({
+            errors: {
+                ...prevstate.errors,
+                name: nameError,
+                address: addressError
+            }
+        }));
+    }
+
+    /* Validate Entire Form */
+    validateForm() {
+        let nameError = false;
+        let addressError = false;
+
+        if (this.isBlank(this.state.name))
+            nameError = true;
+
+        if (this.isBlank(this.state.address))
+            addressError = true;
+
+        if (nameError || addressError) {
+            this.setState(prevstate => ({
+                errors: {
+                    ...prevstate.errors,
+                    name: nameError,
+                    address: addressError,
+                    validate: true
+                }
+            }))
+            return false;
+        }
+        else {
+            this.setState(prevstate => ({
+                errors: {
+                    ...prevstate.errors,
+                    name: nameError,
+                    address: addressError
+                }
+            }));
+            return true;
+        }
+    }
+
+    /* Submit Form */
+    handleSubmit = async event => {
+        event.preventDefault();
+        const path = window.location.pathname;
+        const isEdit = path.includes("edit");
+        //get id -> campus/${campus.id}/edit
+        const id = parseInt(path.split('/').slice(-2, -1)[0]);
+
+        if (!this.validateForm()) {
+            return;
+        }
+
+        const shouldUseImageUrl = (this.state.imageUrl && this.state.imageUrl.trim() !== "") ? true : false;
+        const shouldUseDescription = (this.state.description && this.state.description.trim() !== "") ? true : false;
+
+        let campus = {
+            name: this.state.name,
+            address: this.state.address,
+            ...(shouldUseImageUrl && { imageUrl: this.state.imageUrl }),
+            ...(shouldUseDescription && { description: this.state.description }),
+            id: isEdit ? id : null,
+        }
+
+        let editCampus = await this.props.editCampus(campus);
+
+        this.setState({
+            name: "",
+            description: "",
+            imageUrl: "",
+            address: "",
+            errors: {
+                name: null,
+                address: null,
+                validate: null,
+                shouldDisplayError: false
+            },
+            redirect: true,
+            redirectId: isEdit ? id : editCampus.id,
+            id: null
+        });
+    }
+
+    componentWillUnmount() {
+        this.setState({ redirect: false, redirectId: null });
+    }
+
+    render() {
+        if (this.state.redirect) {
+            return (<Redirect to={`/campus/${this.state.redirectId}`} />)
+        }
+        return (
+            <EditCampusView
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                handleFocus={this.handleFocus}
+                errors={this.state.errors}
+            />
+        )
+    }
+}
+
+const mapDispatch = (dispatch) => {
+    return({
+        addCampus: (campus) => dispatch(addCampusThunk(campus)),
+        fetchCampus: (campusId) => dispatch(fetchCampusThunk(campusId)),
+        editCampus: (campus) => dispatch(editCampusThunk(campus))
+    })
+};
+
+export default connect(null, mapDispatch)(EditCampusContainer);
