962ee29a8f09aa67d74d632a42562865bdf6e1c5
Jungah Ahn <jungah.ahn69@myhunter.cuny.edu>
2021-12-20 17:54:24 -0500

    updaate add student form to accept more inputs. Last thing is to add dropdown for campus input

--- src/components/containers/NewStudentContainer.js
+++ src/components/containers/NewStudentContainer.js
@@ -5,66 +5,278 @@ import { Redirect } from 'react-router-dom';
 import NewStudentView from '../views/NewStudentView';
 import { addStudentThunk } from '../../store/thunks';
 
-
 class NewStudentContainer extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
+  constructor(props){
+      super(props);
+      this.state = {
           firstname: "",
           lastname: "",
-          campusId: null,
+          imageUrl: "",
+          email: "",
+          gpa: "",
           redirect: false,
-          redirectId: null
-        };
-    }
+          redirectId: null,
+          errors: {
+              firstname: null,
+              lastname: null,
+              email: null,
+              gpa: null,
+              validate: null,
+              shouldDisplayError: false
+      }};
+  }
 
-    handleChange = event => {
+  /* Handle Focus for UX */
+  handleFocus = () => {
+      if (!this.state.errors.shouldDisplayError) {
+          this.setState(prevstate => ({
+              errors: {
+                  ...prevstate.errors,
+                  shouldDisplayError: true
+              }
+          }));
+      }
+  }
+
+  /* Helper Function */
+  isBlank(input) {
+      if (input.trim() === "")
+          return true
+      return false
+  }
+
+  /* Input Validation */
+  /*
+  * The first name, last name, and email fields are required.
+  */
+  handleChange = event => {
       this.setState({
-        [event.target.name]: event.target.value
+          [event.target.name]: event.target.value.trim()
       });
-    }
 
-    handleSubmit = async event => {
-        event.preventDefault();
+      let firstnameError = false;
+      let lastnameError = false;
+      let emailError = false;
 
-        let student = {
-            firstname: this.state.firstname,
-            lastname: this.state.lastname,
-            campusId: this.state.campusId
-        };
+      switch(event.target.name) {
+          case "firstname":
+              if (this.isBlank(event.target.value)) {
+                  firstnameError = true;
+              }
+              if (this.isBlank(this.state.email)) {
+                  emailError = true;
+              }
+              if (this.isBlank(event.target.value)) {
+                  lastnameError = true;
+              }
+              break;
+            case "lastname":
+              if (this.isBlank(event.target.value)) {
+                  lastnameError = true;
+              }
+              if (this.isBlank(event.target.value)) {
+                  firstnameError = true;
+              }
+              if (this.isBlank(this.state.email))
+                  emailError = true;
+              break;
+          case "email":
+              if (this.isBlank(event.target.value))
+                  emailError = true;
+              if (this.isBlank(this.state.firstname))
+                  firstnameError = true;
+              if (this.isBlank(this.state.lastname))
+                  lastnameError = true;
+              break;
+          default:
+              return;
+      }
 
-        let newStudent = await this.props.addStudent(student);
+      this.setState(prevstate => ({
+          errors: {
+              ...prevstate.errors,
+              firstname: firstnameError,
+              lastname: lastnameError,
+              email: emailError
+          }
+      }));
+  }
 
-        this.setState({
+  /* Validate Entire Form */
+  validateForm() {
+      let firstnameError = false;
+      let lastnameError = false;
+      let emailError = false;
+
+      if (this.isBlank(this.state.firstname))
+          firstnameError = true;
+
+      if (this.isBlank(this.state.lastname))
+          lastnameError = true;
+
+      if (this.isBlank(this.state.email))
+          emailError = true;
+
+      if (firstnameError || lastnameError || emailError) {
+          this.setState(prevstate => ({
+              errors: {
+                  ...prevstate.errors,
+                  firstname: firstnameError,
+                  lastname: lastnameError,
+                  email: emailError,
+                  validate: true
+              }
+          }))
+          return false;
+      }
+      else {
+          this.setState(prevstate => ({
+              errors: {
+                  ...prevstate.errors,
+                  firstname: firstnameError,
+                  lastname: lastnameError,
+                  email: emailError
+              }
+          }));
+          return true;
+      }
+  }
+
+  /* Submit Form */
+  handleSubmit = async event => {
+      event.preventDefault();
+
+      if (!this.validateForm()) {
+          return;
+      }
+
+      const shouldUseImageUrl = (this.state.imageUrl && this.state.imageUrl.trim() !== "") ? true : false;
+
+      let student = {
+          firstname: this.state.firstname,
+          lastname: this.state.lastname,
+          email: this.state.email,
+          ...(shouldUseImageUrl && { imageUrl: this.state.imageUrl }),
+      }
+
+      let newStudent = await this.props.addStudent(student);
+      console.log(newStudent);
+
+      this.setState({
           firstname: "",
           lastname: "",
-          campusId: null,
+          imageUrl: "",
+          email: "",
+          gpa: "",
+          errors: {
+              firstname: null,
+              lasttname: null,
+              email: null,
+              validate: null,
+              shouldDisplayError: false
+          },
           redirect: true,
           redirectId: newStudent.id
-        });
-    }
+      });
+  }
 
-    componentWillUnmount() {
-        this.setState({redirect: false, redirectId: null});
-    }
+  componentWillUnmount() {
+      this.setState({ redirect: false, redirectId: null });
+  }
 
-    render() {
-        if(this.state.redirect) {
-          return (<Redirect to={`/student/${this.state.redirectId}`}/>)
-        }
-        return (
+  render() {
+      if (this.state.redirect) {
+          return (<Redirect to= {`/student/${this.state.redirectId}`}/>)
+      }
+      return (
           <NewStudentView
-            handleChange = {this.handleChange}
-            handleSubmit={this.handleSubmit}
+              handleChange = {this.handleChange}
+              handleSubmit = {this.handleSubmit}
+              handleFocus = {this.handleFocus}
+              errors = {this.state.errors}
           />
-        );
-    }
+      )
+  }
 }
 
+/* Map Dispatch to Props to Utilize Functional Thunks */
 const mapDispatch = (dispatch) => {
-    return({
-        addStudent: (student) => dispatch(addStudentThunk(student)),
-    })
+  return({
+      addStudent: (student) => dispatch(addStudentThunk(student))
+  })
 }
 
 export default connect(null, mapDispatch)(NewStudentContainer);
+
+// class NewStudentContainer extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//           firstname: "",
+//           lastname: "",
+//           campusId: null,
+//           imageUrl: "",
+//           email: "",
+//           gpa: "",
+//           redirect: false,
+//           redirectId: null
+//         };
+//     }
+
+//     handleChange = event => {
+//       this.setState({
+//         [event.target.name]: event.target.value
+//       });
+//     }
+
+//     handleSubmit = async event => {
+//         event.preventDefault();
+
+//         let student = {
+//             firstname: this.state.firstname,
+//             lastname: this.state.lastname,
+//             campusId: this.state.campusId,
+//             imageUrl: this.state.imageUrl,
+//             email: this.state.email,
+//             gpa: this.state.gpa,
+//         };
+
+//         let newStudent = await this.props.addStudent(student);
+//         console.log(newStudent);
+
+//         this.setState({
+//           firstname: "",
+//           lastname: "",
+//           campusId: null,
+//           imageUrl: "",
+//           email: "",
+//           gpa: null,
+//           redirect: true,
+//           redirectId: newStudent.id
+//         });
+//     }
+
+//     componentWillUnmount() {
+//         this.setState({redirect: false, redirectId: null});
+//     }
+
+//     render() {
+//         if(this.state.redirect) {
+//           return (<Redirect to={`/student/${this.state.redirectId}`}/>)
+//         }
+//         return (
+//           <NewStudentView
+//             handleChange = {this.handleChange}
+//             handleSubmit={this.handleSubmit}
+//           />
+//         );
+//     }
+// }
+
+// const mapDispatch = (dispatch) => {
+//     return({
+//         addStudent: (student) => dispatch(addStudentThunk(student)),
+//     })
+// }
+
+// export default connect(null, mapDispatch)(NewStudentContainer);
--- src/components/views/NewStudentView.js
+++ src/components/views/NewStudentView.js
@@ -1,70 +1,72 @@
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles( () => ({
-  formContainer:{
-    width: '500px',
-    backgroundColor: '#f0f0f5',
-    borderRadius: '5px',
-    margin: 'auto',
-  },
-  title: {
-    flexGrow: 1,
-    textAlign: 'left',
-    textDecoration: 'none'
-  },
-  customizeAppBar:{
-    backgroundColor: '#11153e',
-    shadows: ['none'],
-  },
-  formTitle:{
-    backgroundColor:'#c5c8d6',
-    marginBottom: '15px',
-    textAlign: 'center',
-    borderRadius: '5px 5px 0px 0px',
-    padding: '3px'
-  }
-}));
+import "../styles/NewStudent.css";
 
 const NewStudentView = (props) => {
-  const {handleChange, handleSubmit } = props;
-  const classes = useStyles();
+  const {handleChange, handleSubmit, handleFocus, errors } = props;
 
   return (
-    <div className={classes.root}>
-      <div className={classes.formContainer}>
-        <div className={classes.formTitle}>
-          <Typography style={{fontWeight: 'bold', fontFamily: 'Courier, sans-serif', fontSize: '20px', color: '#11153e'}}>
-            New Student
-          </Typography>
+
+    <div className="new-student-container">
+            <div className="add-student-form-container border">
+                <form noValidate className="needs-validation" onSubmit= {(e) => handleSubmit(e)}>
+                    <h1>Add Student</h1>
+                    <div className="mb-3">
+                        <label className="form-control-label" htmlFor="name">First Name*</label>
+                        <input name="firstname" required pattern=".*\S+.*" title="This field cannot be blank."
+                            className={"form-control " + (errors.shouldDisplayError ? (errors.firstname === false ? "is-valid" : "is-invalid") : "")}
+                            onChange = {(e) => handleChange(e)}
+                            onFocus = {(e) => handleFocus(e)}
+                        />
+                        <div className="valid-feedback">Looks good!</div>
+                        <div className="invalid-feedback">This field is required.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-control-label" htmlFor="name">Last Name*</label>
+                        <input name="lastname" required pattern=".*\S+.*" title="This field cannot be blank."
+                            className={"form-control " + (errors.shouldDisplayError ? (errors.lastname === false ? "is-valid" : "is-invalid") : "")}
+                            onChange = {(e) => handleChange(e)}
+                            onFocus = {(e) => handleFocus(e)}
+                        />
+                        <div className="valid-feedback">Looks good!</div>
+                        <div className="invalid-feedback">This field is required.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-control-label" htmlFor="address">Email*</label>
+                        <input name="email" required pattern=".*\S+.*" title="This field cannot be blank."
+                            className={"form-control " + (errors.shouldDisplayError ? (errors.email === false ? "is-valid" : "is-invalid") : "")}
+                            onChange = {(e) => handleChange(e)}
+                            onFocus = {(e) => handleFocus(e)}
+                        />
+                        <div className="valid-feedback">Looks good!</div>
+                        <div className="invalid-feedback">This field is required.</div>
+                    </div>
+                    {/* <div className="mb-3">
+                    <label for="campuses">Campus Name</label>
+                    <select id="campuses" name="campusId">
+                      <option value="1">Hunter</option>
+                      <option value="2">MIT</option>
+                      <option value="3">Harvard</option>
+                    </select>
+                    </div> */}
+                    <div className="mb-3">
+                        <label className="form-label">Student Image Url</label>
+                        <input name="imageUrl" className="form-control"
+                            onChange = {(e) => handleChange(e)}
+                            onFocus = {(e) => handleFocus(e)}
+                        />
+                        <div className="form-text">eg. https://site.com/student.png</div>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">GPA</label>
+                        <input name="gpa" className="form-control"
+                            onChange = {(e) => handleChange(e)}
+                            onFocus = {(e) => handleFocus(e)}
+                        />
+                    </div>
+                    { errors.validate && <p className="required-text">Invalid inputs, please fix errors.</p>}
+                    <button type="submit" className="btn btn-primary">Add Student</button>
+                </form>
+            </div>
         </div>
-        <form style={{textAlign: 'center'}} onSubmit={(e) => handleSubmit(e)}>
-          <label style= {{color:'#11153e', fontWeight: 'bold'}}>First Name: </label>
-          <input type="text" name="firstname" onChange ={(e) => handleChange(e)} />
-          <br/>
-          <br/>
-
-          <label style={{color:'#11153e', fontWeight: 'bold'}}>Last Name: </label>
-          <input type="text" name="lastname" onChange={(e) => handleChange(e)} />
-          <br/>
-          <br/>
-
-          <label style={{color:'#11153e', fontWeight: 'bold'}}>campusId: </label>
-          <input type="text" name="campusId" onChange={(e) => handleChange(e)} />
-          <br/>
-          <br/>
-
-          <Button variant="contained" color="primary" type="submit">
-            Submit
-          </Button>
-          <br/>
-          <br/>
-        </form>
-        </div>
-      </div>
-
   )
 }
 
